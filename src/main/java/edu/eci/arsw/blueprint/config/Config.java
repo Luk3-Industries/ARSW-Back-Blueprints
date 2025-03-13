e8f0010e460afe901770ad7e1fb507036c92c875
Miguel Motta <93295363+MIGUEL-MOTTA-U@users.noreply.github.com>
2025-03-12 23:55:07 -0500

    feat: update CORS

--- src/main/java/edu/eci/arsw/blueprint/config/Config.java
+++ src/main/java/edu/eci/arsw/blueprint/config/Config.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class Config {
@@ -67,5 +70,21 @@ public class Config {
         }
         return response;
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                logger.info("CORS enabled");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
 
 }
